55f84e169f05c7b2c119a73cfaff238e38310242
kaif8077 <149231751+kaif8077@users.noreply.github.com>
2025-11-23 13:11:26 +0530

    Implement button click to show message
    Added state management to toggle visibility of a message.

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,17 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+
+  let [flag, setFlag] = useState(false)
   return (
     <div id="main">
-      <button id="click"> Click </button>
-      <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+      <button id="click" onClick={() => setFlag(true)}> Click </button>
+      {flag &&   <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
     </div>
   );
 }
 
 
 export default App;
+
